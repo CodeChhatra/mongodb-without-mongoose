9cde9eb057928f2729406447081c5f3edc0a5cdc
code.chhatra <rameshdiwedi022@gmail.com>
2023-12-05 16:43:19 +0530

    que2

--- app.js
+++ app.js
@@ -1,19 +1,83 @@
 const express = require('express');
-const connectAndInsertData = require('./db');
-const usersData = require('./userdata.json').usersData;
-const userProfileData = require('./userprofile.json').userProfileData;
+const { connectDB } = require('./db');
 
 const app = express();
 const PORT = 5000;
 
-app.get('/insertData', async (req, res) => {
+app.get('/averageAge', async (req, res) => {
   try {
-    await connectAndInsertData(usersData, userProfileData);
-    res.send('Data inserted into MongoDB collections.');
+    const db = await connectDB();
+    const usersCollection = db.collection('UsersProfile');
+
+    const users = await usersCollection.find({}).toArray();
+
+    const currentDate = new Date();
+    let totalAge = 0;
+    let validUsersCount = 0;
+
+    for (const user of users) {
+      try {
+        const dob = new Date(user.dob); // Parse MongoDB date format
+
+        const age = calculateAge(dob, currentDate);
+
+        if (!isNaN(age)) {
+          totalAge += age;
+          validUsersCount++;
+
+          
+          await usersCollection.updateOne(
+            { _id: user._id }, 
+            { $set: { age: age } } 
+          );
+        }
+      } catch (error) {
+        console.error(`Error calculating age for user ${user.name}: ${error}`);
+      }
+    }
+
+    const averageAge = totalAge / validUsersCount;
+
+    res.json({ averageAge });
   } catch (err) {
-    res.status(500).send('Error inserting data.');
+    console.log(err);
+    res.status(500).json({ error: 'Error calculating average age.' });
   }
 });
+
+// Function to calculate age
+function calculateAge(dateOfBirth, currentDate) {
+  const dob = new Date(dateOfBirth);
+  const ageDate = new Date(currentDate - dob);
+  const calculatedAge = Math.abs(ageDate.getUTCFullYear() - 1970);
+  return isNaN(calculatedAge) ? null : calculatedAge; 
+}
+
+app.get('/deleteUsers', async (req, res) => {
+  try {
+    const db = await connectDB();
+    const userProfileCollection = db.collection('UsersProfile');
+    const usersCollection = db.collection('Users');
+
+    const ageThreshold = 25; 
+
+    
+    const usersToDelete = await userProfileCollection.find({ age: { $gte: ageThreshold } }).toArray();
+
+   
+    const deleteProfileResult = await userProfileCollection.deleteMany({ age: { $gte: ageThreshold } });
+
+   
+    const userIdsToDelete = usersToDelete.map(user => user.userId); // Assuming there's a userId field in the user profiles
+    const deleteUserResult = await usersCollection.deleteMany({ _id: { $in: userIdsToDelete } });
+
+    res.json({ deletedProfilesCount: deleteProfileResult.deletedCount, deletedUsersCount: deleteUserResult.deletedCount });
+  } catch (err) {
+    res.status(500).json({ error: 'Error deleting users.' });
+  }
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Node.js App running on port ${PORT}...`);
--- db.js
+++ db.js
@@ -3,29 +3,44 @@ const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://rahul:Yg6i3prvrBgSF4ay@cluster0.io54nr3.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(uri);
 
-async function connectAndInsertData(usersData, userProfileData) {
-  try {
-    await client.connect();
-    // console.log('Connected to MongoDB!');
+// async function connectAndInsertData(usersData, userProfileData) {
+//   try {
+//     await client.connect();
+//     console.log('Connected to MongoDB!');
 
-    const db = client.db('test');
-    const usersCollection = db.collection('Users');
-    const userProfileCollection = db.collection('UsersProfile');
+//     const db = client.db('test');
+//     const usersCollection = db.collection('Users');
+//     const userProfileCollection = db.collection('UsersProfile');
 
-    // Insert users data into Users collection
-    await usersCollection.insertMany(usersData);
-    console.log('Users data inserted.');
+//     // Insert users data into Users collection
+//     await usersCollection.insertMany(usersData);
+//     console.log('Users data inserted.');
 
-    // Insert user profile data into UsersProfile collection
-    await userProfileCollection.insertMany(userProfileData);
-    console.log('User profiles data inserted.');
-  } catch (err) {
-    console.error('Error inserting data:', err);
-    console.error(err.stack);
-  } finally {
-    await client.close();
-    console.log('Connection closed.');
+//     // Insert user profile data into UsersProfile collection
+//     await userProfileCollection.insertMany(userProfileData);
+//     console.log('User profiles data inserted.');
+//   } catch (err) {
+//     console.error('Error inserting data:', err);
+//     console.error(err.stack);
+//   } finally {
+//     await client.close();
+//     console.log('Connection closed.');
+//   }
+// }
+
+async function connectDB() {
+    try {
+      await client.connect();
+      console.log('Connected to MongoDB!');
+      return client.db('test');
+    } catch (err) {
+      console.error('Error connecting to the database:', err);
+      throw err;
+    }
   }
-}
 
-module.exports = connectAndInsertData;
+  
+  
+  module.exports = { connectDB };
+
+// module.exports = connectAndInsertData;
